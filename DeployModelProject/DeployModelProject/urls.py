eb827cb9352f4078699781682ca16f80436be314
NEWTON <newton@kali>
2021-04-16 06:33:39 +0300

    fits commit

--- DeployModelProject/DeployModelProject/urls.py
+++ DeployModelProject/DeployModelProject/urls.py
@@ -4,6 +4,7 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home'),
+    path('', views.index, name = 'index'),
+    path('home/', views.home, name='home'),
     path('results/', views.results, name = 'results')
 ]
--- DeployModelProject/DeployModelProject/views.py
+++ DeployModelProject/DeployModelProject/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def index(request):
+    return render(request,'index.html')
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 def results(request):
     return render(request, 'results.html')
